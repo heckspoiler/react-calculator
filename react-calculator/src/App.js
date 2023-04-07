478c4422bc37fc8113bf8b4bda77e0f07275853c
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-08 01:20:46 +0200

    finished

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -122,6 +122,7 @@ function formatOperand(operand) {
   if (operand == null) return;
   const [integer, decimal] = operand.split(".");
   if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 }
 
 function App() {
